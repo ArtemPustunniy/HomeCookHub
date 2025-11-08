9d265e88c987d6249f8aa6a5f94fd804a5827ef3
ArtemPustunniy <artem1234562005@gmail.com>
2025-11-08 16:25:42 +0300

    fixed vite.config.ts for ghp

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/homecookhub/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/HomeCookHub/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
